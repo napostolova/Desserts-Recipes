500b262253605cbc75d91fe13d28d7c8587634ab
Nadya Apostolova <napostolova01@gmail.com>
2021-08-13 13:38:37 +0300

    Added error component

---
+++ src/app/error/error.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-error',
+  templateUrl: './error.component.html',
+  styleUrls: ['./error.component.css']
+})
+export class ErrorComponent {
+
+  constructor(
+
+    private router: Router) {
+       
+    }
+  
+  goToHomePage(): void {
+    this.router.navigate(['/'])
+  }
+}
